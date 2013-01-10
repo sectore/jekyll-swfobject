ae35389338698efe17098fc837afbefcbba9429d
Jens Krause <info@websector.de>
2013-01-10 10:44:31 +0100

    initial commit + initial source

---
+++ README.md
@@ -0,0 +1,126 @@
+# Jekyll Plugin: SWFObject Tag
+
+SWFObject Tag is a [Liquid](http://www.liquidmarkup.org/) plugin for [Jekyll](http://jekyllrb.com/) to embed Flash files (*.swf) using [SWFObject](http://code.google.com/p/swfobject/)
+
+## Installation
+
+1. Copy `swfobject_tag.rb` (folder: _plugins) into `<your-jekyll-project>/_plugins` folder.
+
+2. Link SWFObject at the layout template or page in `<head>` or before `{% swfobject %}` :
+
+    `<script src="//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>`
+
+
+## Example
+
+### Tag
+    {% swfobject swf_url %}{% endswfobject %}
+Note: Because SWFObject Tag is a `Liquid::Block`, you have to close any `{% swfobject %}` with `{% endswfobject %}` in your templates.
+
+#### Optional parameters
+
+    {% swfobject  swf_url, id: myFlash, content_id: flashcontent, width: 500, height: 600  %}{% endswfobject %}
+
+For all optional parameters check section [Parameters](#params) below.
+
+
+#### Alternative HTML content
+
+    {% swfobject swf_url %}
+    <p>Latest <a href='http://www.adobe.com/go/getflashplayer'>Flash Player Plugin</a> is required.</p>
+    {% endswfobject %}
+
+#### Optional parameters defined in `_config.yml`
+
+
+    swfobject:
+      height: 350
+      width: 200
+      express_install_url: expressInstall.swf
+      alternative_content: <p>Please install latest <a href='http://www.adobe.com/go/getflashplayer'>Flash Player Plugin</a>.</p>
+
+For all optional parameters check section [Parameters](#params) below.
+
+
+## Parameters ##  {#params}
+
+### Required parameter
+
+`swf_url` - Path to SWF, which has to be embedded
+
+### Optional parameters
+
+All optional parameters can be defined within `{% swfobject %}` and / or using `_config.yml`.
+Parameters defined in `_config.yml` will be overridden by tag parameters.
+
+All the following parameters are optional.
+
+`content_id` - Id of the div container, which will be replaced by SWFObject to embed SWF
+
+`height` - Width of the embedded SWF (default: 100%)
+
+`width` - Height of the embedded SWF (default: 100%)
+
+`version` - Version of the Flash Player Plugin (default: 10.0)
+
+`flashvars` - String of flashvars using `key=value` pairs combinend with `&`, e.g. `name1=hello&name2=world`
+
+`express_install_url` - URL of the expressInstall.swf
+
+`callback_function` - Callback function to check success or failure of creating a Flash plug-in `<object>`
+
+
+#### Optional Attributes of the `<object>` element
+
+`id`
+
+`align`
+
+`name`
+
+`styleclass`
+
+For detailed information check SWF Object documentation: http://code.google.com/p/swfobject/wiki/documentation
+
+#### Optional Flash specific parameters
+
+`play`
+
+`loop`
+
+`menu`
+
+`quality`
+
+`scale`
+
+`salign`
+
+`wmode`
+
+`bgcolor`
+
+`base`
+
+`swliveconnect`
+
+`devicefont`
+
+`allowscriptaccess`
+
+`seamlesstabbing`
+
+`allowfullscreen`
+
+`allownetworking`
+
+For detailed information check SWF Object documentation: http://code.google.com/p/swfobject/wiki/documentation
+
+
+## Author
+
+Jens Krause // [http://websector.de](http://websector.de)
+
+## License
+
+Code is licensed under [MIT](http://opensource.org/licenses/MIT)
---
+++ _plugins/swfobject_tag.rb
@@ -0,0 +1,172 @@
+# SWFObject Tag
+# is a Jekyll plugin to embed Flash files (*.swf) using SWFObject http://code.google.com/p/swfobject/
+#
+# Example:
+#
+#   {% swfobject swf_url %}{% endswfobject %}
+# or
+#   {% swfobject  swf_url, id: swfID, content_id: flashcontent, width: 500, height: 600  %}{% endswfobject %}
+# or
+#   {% swfobject swf_url %}
+#   <p>Alternative HTML content, e.g. Latest <a href='http://www.adobe.com/go/getflashplayer'>Flash Player Plugin</a> is required.</p>
+#   {% endswfobject %}
+#
+# Full documentation:
+#  https://github.com/sectore/jekyll-swfobject/blob/master/README.md
+#
+# Author: Jens Krause http://websector.de
+# Source: https://github.com/sectore/jekyll-swfobject
+# License: MIT
+
+
+
+module Jekyll
+
+  class SWFObjectTag < Liquid::Block
+
+    ATTRIBUTES = [
+      'id', 'align', 'name', 'styleclass'
+    ]
+
+    PARAMETERS = [
+      'play', 'loop', 'menu', 'quality', 'scale', 'salign', 'wmode', 'bgcolor', 'base',
+      'swliveconnect','devicefont','allowscriptaccess','seamlesstabbing','allowfullscreen', 'allownetworking'
+    ]
+
+    DEFAULTS = {
+      "content_id" => "flashcontent",
+      "width" => "100%",
+      "height" => "100%",
+      "version" => "10.0",
+      "express_install_url" => "null",
+      "callback_function" => "null",
+      "flashvars" => "",
+      "alternative_content" => "<p>Please install latest <a href='http://www.adobe.com/go/getflashplayer'>Flash Player Plugin</a>.</p>"
+    }
+
+    def initialize(tag_name, text, tokens)
+      super
+
+      @config = {}
+      # set defaults
+      override_config(DEFAULTS)
+
+      # override configuration with values defined within _config.yml
+      if Jekyll.configuration({}).has_key?('swfobject')
+        config = Jekyll.configuration({})['swfobject']
+        override_config(config)
+      end
+
+      params = text.split
+
+      # first argument (required) is url of swf
+      @swf_url = params.shift.strip
+
+      if params.size > 0
+        # override configuration with parameters defined within {% swfobject pathToSWF %}
+        config = {} # reset local config
+        params.each do |param|
+          param = param.gsub /\s+/, '' # remove whitespaces
+          key, value = param.split(':',2) # split first occurrence of ':' only
+          config[key] = value
+        end
+        override_config(config)
+      end
+
+    end
+
+    def override_config(config)
+      config.each{ |key,value| @config[key] = value }
+    end
+
+    def render(context)
+      output = super
+      #<<-HTML
+      <<-HTML.gsub /^\s+/, '' # remove whitespaces from heredocs
+      <div id="#{@config['content_id']}" style="#{render_content_style()}">#{render_alternative_content(output)}</div>
+      <script type="text/javascript">
+        //  <![CDATA[
+        #{render_flashvars()}
+      #{render_params()}
+      #{render_attributes()}
+        swfobject.embedSWF(
+          '#{@swf_url}',
+          '#{@config['content_id']}',
+          '#{@config['height']}',
+          '#{@config['width']}',
+          '#{@config['version']}',
+          '#{@config['express_install_url']}',
+          flashvars,
+          params,
+          attributes,
+          '#{@config['callback_function']}'
+        );
+        // ]]>
+      </script>
+      HTML
+    end
+
+    def render_content_style()
+      result = "width:"
+      width = @config['width']
+      width.include? '%' ? (result += width+";") : (result +=width+"px;")
+
+      result += "height:"
+      height = @config['height']
+      height.include? '%' ? (result += height+";") : (result +=height+"px;")
+      result += ";"
+    end
+
+    def render_alternative_content(output)
+      unless output.strip.size > 0
+        output = @config['alternative_content']
+      end
+      output
+    end
+
+    def render_params
+      result = "var params = {"
+      @config.each do |key, value|
+        if PARAMETERS.member? key
+          result += " #{key}: '#{value}',"
+        end
+      end
+      result = remove_last_comma(result)
+      result += "};"
+    end
+
+    def render_attributes
+      result = "var attributes = {"
+      @config.each do |key, value|
+        if ATTRIBUTES.member? key
+          result += " #{key}: '#{value}',"
+        end
+      end
+      result = remove_last_comma(result)
+      result += "};"
+    end
+
+    def render_flashvars
+      result = "var flashvars = {"
+
+      if !@config['flashvars'].nil?
+        vars = @config['flashvars'].split('&')
+        vars.each do |var|
+          key_value = var.split('=',2) # split first occurrence of '=' only
+          key = key_value.first
+          value = key_value.last
+          result += " #{key}: '#{value}',"
+        end
+        result = remove_last_comma(result)
+      end
+      result += "};"
+    end
+
+    def remove_last_comma(content)
+      content.gsub(/,?$/, '')
+    end
+
+  end
+end
+
+Liquid::Template.register_tag('swfobject', Jekyll::SWFObjectTag)
